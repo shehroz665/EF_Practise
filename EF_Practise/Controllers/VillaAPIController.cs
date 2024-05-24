f45a38b5552e5a94c026757b24cb8ee58d06a619
shehroz665 <shehrozmalik665@gmail.com>
2024-05-24 02:28:38 +0100

    Add project files.

---
+++ EF_Practise/Controllers/VillaAPIController.cs
@@ -0,0 +1,22 @@
+﻿using EF_Practise.Modals;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EF_Practise.Controllers
+{
+    [Route("api/villaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public List<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa { Id = 1, Name = "A" },
+                new Villa { Id = 2, Name = "B" },
+                new Villa { Id = 3, Name = "C" },
+            };
+        }
+    }
+}
---
+++ EF_Practise/Modals/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace EF_Practise.Modals
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
