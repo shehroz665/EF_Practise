c7455be7802f67000e0bdabda16dc10843012b89
shehroz665 <shehrozmalik665@gmail.com>
2024-05-27 02:59:41 +0100

    Worked on VillaApi Controller

--- EF_Practise/Controllers/VillaAPIController.cs
+++ EF_Practise/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using EF_Practise.Modals;
+﻿using EF_Practise.Data;
+using EF_Practise.Modals;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,12 +12,68 @@ namespace EF_Practise.Controllers
         [HttpGet]
         public List<Villa> GetVillas()
         {
-            return new List<Villa>()
+            
+            return VillaStore.villas;
+        }
+
+        [HttpGet("id")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public async Task<ActionResult<Villa>> GetVilla([FromQuery] int id)
+        {
+            if(id== 0)
             {
-                new Villa { Id = 1, Name = "A" },
-                new Villa { Id = 2, Name = "B" },
-                new Villa { Id = 3, Name = "C" },
-            };
+                return BadRequest();
+            }
+            var villa = VillaStore.villas.FirstOrDefault(item => item.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        [HttpDelete("id")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public async Task<ActionResult<Villa>> DeleteVilla([FromQuery] int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villas.FirstOrDefault(item => item.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villas.Remove(villa);
+            return Ok(villa);
+        }
+
+        [HttpGet("name")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+
+        public async Task<ActionResult<List<Villa>>> SearchVilla([FromQuery] string? search)
+        {
+            if (string.IsNullOrEmpty(search))
+            {
+                return Ok(VillaStore.villas.ToList());  
+            }
+            var villa= VillaStore.villas
+                .Where(item=> item.Name.Contains(search,StringComparison.OrdinalIgnoreCase))
+                .ToList();
+            if (villa == null || villa.Count==0)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
         }
     }
 }
---
+++ EF_Practise/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using EF_Practise.Modals;
+
+namespace EF_Practise.Data
+{
+    public static class VillaStore
+    {
+        public static List<Villa> villas = new List<Villa>{ 
+        new Villa{Id=1,Name="FSD"},
+        new Villa{Id=2,Name="LHR"},
+        new Villa{Id=3,Name="ISL"},
+        new Villa{Id=4,Name="KHI"},
+        };
+    }
+}
